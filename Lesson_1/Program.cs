b3e660e1d6af0a7ece4a083467192d13f4021685
Nikolay Astashenko <astashenko3@gmail.com>
2023-10-21 23:01:48 +0300

    Lesson 1

---
+++ Lesson_1/Program.cs
@@ -0,0 +1,90 @@
+﻿// Выводит квадрат целого числа
+
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = num * num;
+// Console.WriteLine(result);
+
+
+
+
+// Программа, которая на вход принемает
+// два числа, и проверяет, является ли
+// первое квадратом второго
+
+// Console.WriteLine("Введите первое число:");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число:");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// if(num1 == num2 * num2)
+// {
+//     Console.WriteLine("Да");
+// }
+// else
+// {
+//     Console.WriteLine("Нет");
+// }
+
+
+
+// Программа выдает день недели по
+// по заданному номеру.
+
+// Console.WriteLine("Введите номер дня недели");
+// int num = Convert.ToInt32(Console.ReadLine());
+// if (num == 1)
+// {
+//     Console.WriteLine("Понедельник");
+// }
+// else if (num == 2)
+// {
+//     Console.WriteLine("Вторник");
+// }
+// else if (num == 3)
+// {
+//     Console.WriteLine("Среда");
+// }
+// else if (num == 4)
+// {
+//     Console.WriteLine("Четверг");
+// }
+// else if (num == 5)
+// {
+//     Console.WriteLine("Пятница");
+// }
+// else if (num == 6)
+// {
+//     Console.WriteLine("Суббота");
+// }
+// else if (num == 7)
+// {
+//     Console.WriteLine("Воскресенье");
+// }
+// else
+// {
+//     Console.WriteLine("Не верно, введите номер дня недели");
+// }
+
+
+
+// Принимает на вход одно число (N), а на выходе
+// показывает все целые числа от -N до N
+
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int negNum = num * -1;
+// while(negNum <= num)
+// {
+//    Console.WriteLine(negNum);
+//    negNum++;
+// }
+
+
+
+// Программа принемает на вход трехзначенное число
+// и на выходе показывает последнюю цифру этого числа.
+
+// Console.WriteLine("Введите число:");
+// int num = Convert.ToInt32(Console.ReadLine());
+// int result = num % 10;
+// Console.WriteLine($"Нужное число {result}");
